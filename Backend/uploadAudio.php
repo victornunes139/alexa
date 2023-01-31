cf2436b0fb2dcb6e61721413ae2eaa0417ae6ebd
Victor Nunes <victor.nunes@opovodigital.com>
2023-01-31 16:23:43 -0300

    Add the feature to receive form audios and move them to an internal folder

--- Backend/uploadAudio.php
+++ Backend/uploadAudio.php
@@ -1,9 +1,23 @@
 <?php
 
+$url = 'http://localhost';
+
+if(isset($_FILES['audio']) && is_uploaded_file($_FILES['audio']['tmp_name'])) {
+    $uploads_dir = '../uploads';
+    $tmp_name = $_FILES['audio']['tmp_name'];
+    $name = basename($_FILES["audio"]["name"]);
 
 
+    ///Deletar um arquivo
+    $files = glob('../uploads/*');
+    foreach($files as $file) { 
+        if(is_file($file)) {
+            unlink($file); 
+        }
+    }
 
 
-if(isset($_POST['file'])) {
-    var_dump($_POST['file']);
+    move_uploaded_file($tmp_name, "$uploads_dir/$name");
+} else {
+    header('Location: ' . $url);
 }
